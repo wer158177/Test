7647dfc6a421f9c3e5a44daaef3d5de9c0b4c6cd
조인욱 <wer158177@gmail.com>
2024-11-18 11:12:47 +0900

    test2

--- src/main/java/com/sparta/memo/PostApplication.java
+++ src/main/java/com/sparta/memo/PostApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MemoApplication {
+public class PostApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MemoApplication.class, args);
+        SpringApplication.run(PostApplication.class, args);
     }
 
 }
